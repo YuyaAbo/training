a164311f5790720fba52bea9d764bd114a7d8b1a
YuyaAbo <b1011211@gmail.com>
2015-07-19 16:21:08 +0900

    add: calc()追加

--- script/calc.js
+++ script/calc.js
@@ -1,49 +1,98 @@
-var result	= 0;
-var num		= 0;
-var key		= "";
-var kigou	= "";
+var result	= 0;	// 一つ前の計算結果
+var num		= '';	// 一つ前の計算記号
+var preKey	= '';	// 直前に入力された記号
+var nextKey	= '';	// 次の計算のための記号
 
 function numBtnPush(btn){
-	if(num == 0){
+	if (num == '') {
 		num = btn;
-	}else{
-		num += ""+btn;
+	} else {
+		num += ''+btn;
 	}
 
-	key = btn;
+	//key = btn;
 
 	console.log("num = %d", num);
-	document.getElementById("output").innerHTML = num;
+	document.getElementById("result").innerHTML = num;
 }
 
 function allClear(){
-	key = "";
+	key = '';
 	result = 0;
-	num = "0";
+	num = 0;
 
 	console.log("Clear");
-	document.getElementById("output").innerHTML = num;
+	document.getElementById("result").innerHTML = num;
 }
 
+function calc(key, result, num) {
+	switch (key) {
+		case '+':
+			return result += Number(num);
+			break;
+		case '-':
+			return result -= Number(num);
+			break;
+		case '*':
+			return result *= Number(num);
+			break;
+		case '/':
+			return result /= Number(num);
+			break;
+		default:
+			return 0;
+			break;
+	}
+}
+
+// 足し算
 function add(){
-	if (isFinite(key)) {
-		result += Number(num);
-		num = 0;
+	if (preKey == '') {
+		preKey = nextKey = '+';
+		result = Number(num);
+		num = '';
+	} else {
+		preKey = nextKey;
+		result = calc(preKey, result, num);
+		num = '';
+		nextKey = '+';
+		console.log("result = %d", result);
+		document.getElementById("result").innerHTML = result;
 	};
-	key = "+";
 
-	console.log("num = %d",num);
-	console.log("result = %d",result);
+	console.log("(num,result) = (%d,%d)\n(preKey,nextKey) = (%s,%s)", num, result, preKey, nextKey);
 
-	//document.getElementById("output").innerHTML = result;
+	document.getElementById("type").innerHTML = nextKey;
 }
 
+// 引き算
+function sub(){
+	if (preKey == '') {
+		preKey = nextKey = '-';
+		result = Number(num);
+		num = '';
+	} else {
+		preKey = nextKey;
+		result = calc(preKey, result, num);
+		num = '';
+		nextKey = '-';
+		console.log("result = %d", result);
+		document.getElementById("result").innerHTML = result;
+	};
+
+	console.log("(num,result) = (%d,%d)\n(preKey,nextKey) = (%s,%s)", num, result, preKey, nextKey);
+
+	document.getElementById("type").innerHTML = nextKey;
+}
+
+// イコール
 function equal(){
 	if (isFinite(key)) {
 		result += Number(num);
 		num = 0;
+		key = "";
 		console.log("result = %d",result);
-		document.getElementById("output").innerHTML = result;
+		document.getElementById("result").innerHTML = result;
 	};
 	key = "E";
 }
